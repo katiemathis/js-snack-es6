d6f8c9db5e30a671e62f8ef535bb84844c09d114
katiepalumbo <palumbo.katie@gmail.com>
2022-02-06 22:42:22 +0100

    snack 1 completata

---
+++ js snack 1/main.js
@@ -0,0 +1,41 @@
+//Creare un array di oggetti descrivendo nome e peso
+
+const bici = [
+    {
+        nome: 'Alan Corsa',
+        peso: 2,
+    },
+
+    {
+        nome: 'Bianchi Specialissima',
+        peso: 7,
+    },
+
+    {
+
+        nome: 'Huffy Cruiser',
+        peso: 15,
+    }
+
+
+];
+
+function biciPiuLeggero(myArrayOfBici) {
+    let {peso,nome} = myArrayOfBici[0];
+
+
+    for(let i=0; i<myArrayOfBici.length;i++) {
+        if (peso > myArrayOfBici[i].peso){
+            peso = myArrayOfBici[i].peso;
+            nome = myArrayOfBici[i].nome;
+        }
+    }
+
+    let risultato= `La bici più leggera è la ${nome} che pesa soltanto ${peso} kg.`;
+
+    return risultato;
+
+};
+
+let consoleMessage = biciPiuLeggero(bici);
+console.log(consoleMessage);
